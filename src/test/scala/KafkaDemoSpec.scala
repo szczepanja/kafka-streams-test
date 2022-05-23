16609d73257fe386caeb51c17f8156e77a92595b
a_szczepanik <a.szczepanik@adform.com>
2022-05-23 11:56:27 +0200

    Add succeeding tests

--- src/test/scala/KafkaDemoSpec.scala
+++ src/test/scala/KafkaDemoSpec.scala
@@ -15,24 +15,21 @@ class KafkaDemoSpec extends AnyFlatSpec with should.Matchers {
     val testDriver = new TopologyTestDriver(topology)
     val inputTopic: TestInputTopic[String, String] = testDriver.createInputTopic(KafkaDemo.INPUT_TOPIC, stringSerde.serializer, stringSerde.serializer())
     val outputTopic: TestOutputTopic[String, String] = testDriver.createOutputTopic(KafkaDemo.OUTPUT_TOPIC, stringSerde.deserializer, stringSerde.deserializer)
-    val stateStore = testDriver.getAllStateStores
-      .asScala.head._2.asInstanceOf[MeteredKeyValueStore[String, ValueAndTimestamp[Long]]]
-
-    testDriver.getAllStateStores
-      .asScala.foreach(println)
-
 
     inputTopic.pipeInput("value value ania ma kota i psa")
     outputTopic.readKeyValue() shouldBe KeyValue.pair("value", "1")
   }
 
-  it should "return value of store values" in {
+  it should "return value of stored values" in {
     val topology: Topology = KafkaDemo.getTopology
     val testDriver = new TopologyTestDriver(topology)
     val inputTopic: TestInputTopic[String, String] = testDriver.createInputTopic(KafkaDemo.INPUT_TOPIC, stringSerde.serializer, stringSerde.serializer())
-    val outputTopic: TestOutputTopic[String, String] = testDriver.createOutputTopic(KafkaDemo.OUTPUT_TOPIC, stringSerde.deserializer, stringSerde.deserializer)
-    val stateStore = testDriver.getAllStateStores
-      .asScala.head._2.asInstanceOf[MeteredKeyValueStore[String, ValueAndTimestamp[Long]]]
+    val stateStore: MeteredKeyValueStore[String, ValueAndTimestamp[Long]] = testDriver
+      .getAllStateStores
+      .asScala
+      .head
+      ._2
+      .asInstanceOf[MeteredKeyValueStore[String, ValueAndTimestamp[Long]]]
 
     inputTopic.pipeInput("value value ania ma kota i psa")
     stateStore.get("value").value() shouldBe 2
